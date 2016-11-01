cbe5deb89586d916ddcc06248f52bbafc9305a84
梁飞涛 <iMadarax@Madarax-MACPRO.local>
2016-11-01 23:35:51 +0800

    first.commit

---
+++ 7🐟/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  7🐟
+//
+//  Created by 梁飞涛 on 2016/11/1.
+//  Copyright © 2016年 梁飞涛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.cyan
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
