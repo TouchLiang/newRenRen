fed927ba7f1f6be98c97bfd8b77bb2c50849045b
TouchLiang <madarax@163.com>
2016-11-07 18:15:17 +0800

    Touch:myButton

---
+++ 7🐟/MYButtonController.swift
@@ -0,0 +1,24 @@
+//
+//  MYButtonController.swift
+//  7🐟
+//
+//  Created by 梁飞涛 on 2016/11/4.
+//  Copyright © 2016年 梁飞涛. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MYButtonController : UIViewController{
+    
+    override func viewDidLoad() {
+        
+        title = "自定义button"
+    }
+}
+
+class MYButton : UIButton {
+    
+    
+    
+}
--- 7🐟/ViewController.swift
+++ 7🐟/ViewController.swift
@@ -8,33 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let TITLE_1 = "这是一个Title"
+let TITLE_2 = "这是一个Title x 2"
+let TITLE_3 = "这是一个Title x 3"
+let MYBUTTON = "自定义Button"
 
+class ViewController: UIViewController ,UITableViewDataSource ,UITableViewDelegate{
+
+    var tableDataSource = [TITLE_1,TITLE_2,TITLE_3,MYBUTTON]
+    private var tableView : UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.cyan
         
-        let title = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
-        title.text = "这是一个Title"                
-        title.sizeToFit()
-        title.center = view.center
-        view.addSubview(title)
-        
-        let title2 = UILabel(frame: CGRect(x: 0, y: 0, width: title.frame.size.width, height: title.frame.size.height))
-        title2.text = "这是一个Title x 2"
-        title2.sizeToFit()
-        title2.center = CGPoint(x: title.center.x, y: title.center.y + 40)
-        view.addSubview(title2)
-        
-        let title3 = UILabel(frame: CGRect(x: 0, y: 0, width: title.frame.size.width, height: title.frame.size.height))
-        title3.text = "这是一个Title x 3"
-        title3.sizeToFit()
-        title3.center = CGPoint(x: title.center.x, y: title2.center.y + 40)
-        view.addSubview(title3)
-        
+        tableView = UITableView(frame: view.bounds)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
     }
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableDataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = tableDataSource[indexPath.row]
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(tableDataSource[indexPath.row])
+        
+        if tableDataSource[indexPath.row] == MYBUTTON{
+            navigationController?.pushViewController(MYButtonController(), animated: true)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
